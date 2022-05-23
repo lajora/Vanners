61fb4ee22dcb97e3b691359bc4f398511698703f
Lara Keen <lara.jr.keen@gmail.com>
2022-05-23 14:51:51 +0200

    created schema, models & controllers, views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  # get 'bookings/index'
+  # get 'bookings/show'
+  # get 'vans/index'
+  # get 'vans/show'
+  # get 'vans/new'
+  # get 'vans/create'
+  devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :vans, except: [:edit, :update, :destroy] do
+    resources :bookings, only: :new
+  end
+  resources :bookings, only: [:index, :show]
 end
