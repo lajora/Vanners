3da103cd817c25f9d399c12a2eec7991ea0b6a59
Laura Gil <lauracodesnow@gmail.com>
2022-05-23 17:54:45 +0200

    Fix the issue with the log in

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,6 @@ Rails.application.routes.draw do
   # get 'vans/create'
   devise_for :users
   root to: 'pages#home'
-  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :vans, except: [:edit, :update, :destroy] do
     resources :bookings, only: :new
