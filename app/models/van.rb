4e128c3c7a0fe1085489b86862c54912484492ee
Judith van Leersum <judithvleersum@gmail.com>
2022-05-25 11:50:42 +0200

    Updated seeds and added validation for address

--- app/models/van.rb
+++ app/models/van.rb
@@ -3,6 +3,11 @@ class Van < ApplicationRecord
   belongs_to :user
   has_many :bookings
   has_many :users, through: :bookings
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
+
   validates :title, :description, :price, presence: true
   validates :description, length: { minimum: 20 }
+  validates :address, presence: true
 end
