b472e15fff157969d47e268d187899f1f53c70c2
Judith van Leersum <judithvleersum@gmail.com>
2022-05-25 13:02:21 +0200

    Updated marker to vanners van logo

--- app/controllers/vans_controller.rb
+++ app/controllers/vans_controller.rb
@@ -5,7 +5,8 @@ class VansController < ApplicationController
       {
         lat: van.latitude,
         lng: van.longitude,
-        info_window: render_to_string(partial: "info_window", locals: { van: van })
+        info_window: render_to_string(partial: "info_window", locals: { van: van }),
+        image_url: helpers.asset_url("van-logo-red.png")
       }
     end
   end
